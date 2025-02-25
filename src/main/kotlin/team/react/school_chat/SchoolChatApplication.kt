e2e1f74211f4a9f6d0234b933228a9a023d4ac2e
Tech5818 <pqow5818@gmail.com>
2025-02-25 23:54:35 +0900

    Init: Project Init

---
+++ src/main/kotlin/team/react/school_chat/SchoolChatApplication.kt
@@ -0,0 +1,11 @@
+package team.react.school_chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<SchoolChatApplication>(*args)
+}
---
+++ src/test/kotlin/team/react/school_chat/SchoolChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.react.school_chat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
