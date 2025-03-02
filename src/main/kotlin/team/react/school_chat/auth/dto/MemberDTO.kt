8e3abb0c915f8bb68a071a67e48831ec238568bd
Tech5818 <pqow5818@gmail.com>
2025-03-03 01:29:09 +0900

    Refactor: Member, Friend DTO 설계

--- src/main/kotlin/team/react/school_chat/auth/dto/MemberDTO.kt
+++ src/main/kotlin/team/react/school_chat/auth/dto/MemberDTO.kt
@@ -5,7 +5,29 @@ import team.react.school_chat.auth.domain.Member
 data class MemberDTO(
     val email: String,
     val name: String,
-    val picture: String
+    val picture: String,
+    val friends: List<FriendDTO> = listOf(),
 ) {
-    fun toEntity(): Member = Member(email, name, picture)
+    companion object {
+        fun from(member: Member): MemberDTO =
+            MemberDTO(
+                email = member.email,
+                name = member.name,
+                picture = member.picture,
+                friends = member.friends.map { FriendDTO.from(it) }
+            )
+    }
+
+    fun toEntity(): Member {
+        val member = Member(
+            email = email,
+            name = name,
+            picture = picture,
+            friends = mutableListOf()
+        )
+
+        member.friends = friends.map { it.toEntity(member) }.toMutableList()
+
+        return member
+    }
 }
