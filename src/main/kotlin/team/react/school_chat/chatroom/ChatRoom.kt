22475306e5213b7604c396133800a8c30fef8350
Lee-jeong-wook <b01024134020@gmail.com>
2025-03-03 19:14:35 +0900

    Feat: web socket routing

---
+++ src/main/kotlin/team/react/school_chat/chatroom/ChatRoom.kt
@@ -0,0 +1,8 @@
+package team.react.school_chat.chatroom
+
+data class ChatRoom (
+        val id : String? = null,
+        val name : String? = null,
+        val email: String? = null,
+        var age: Int? = null
+)
