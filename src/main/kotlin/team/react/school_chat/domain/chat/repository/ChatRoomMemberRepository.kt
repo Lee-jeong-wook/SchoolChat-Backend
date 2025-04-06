73dede7231b7e822b3a374043e4cb1b88bea5553
Lee-jeong-wook <b01024134020@gmail.com>
2025-04-06 17:05:57 +0900

    fix: ChatRoomMemberRepository findAllChatRoomByMemberEmail 쿼리문 변경

--- src/main/kotlin/team/react/school_chat/domain/chat/repository/ChatRoomMemberRepository.kt
+++ src/main/kotlin/team/react/school_chat/domain/chat/repository/ChatRoomMemberRepository.kt
@@ -9,7 +9,7 @@ import team.react.school_chat.domain.chat.domain.ChatRoomMember
 @Repository
 interface ChatRoomMemberRepository : JpaRepository<ChatRoomMember, Long> {
 
-    @Query("SELECT c FROM ChatRoom c JOIN ChatRoomMember m WHERE m.member.email = :memberEmail")
+    @Query("SELECT c FROM ChatRoom c JOIN ChatRoomMember m on c.id = m.chatRoom.id WHERE m.member.email = :memberEmail")
     fun findAllChatRoomByMemberEmail(memberEmail: String): List<ChatRoom>
 
 }
